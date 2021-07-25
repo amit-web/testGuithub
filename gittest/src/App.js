1fc3f8f521bb8a1a7336038f2c7d7fed81eebc4a
amit-web <mehtaamit9334@gmail.com>
2021-07-25 22:30:57 +0530

    My test file

---
+++ gittest/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchData from "./Amit/SearchData"
+function App() {
+  return (
+    <div className="App">
+      <SearchData/>
+    </div>
+  );
+}
+
+export default App;
