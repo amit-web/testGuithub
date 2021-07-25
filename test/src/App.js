8b3fca2485285db539ae6e0f83477bef54aae3c3
NOOR <noormuhammed14@gmail.com>
2021-07-25 22:26:19 +0530

    upload test

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayJobs from "./Compoenents/DisplayJobs"
+function App() {
+  return (
+   <>
+   
+   < DisplayJobs/>
+   </>
+  );
+}
+
+export default App;
